6bd1fa79999f8e45b559b77a0c7601971b03ef6c
NoahNua <NoahNua@hawaii.edu>
2021-02-18 10:44:43 -1000

    update

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -34,23 +34,29 @@ class ViewController: UIViewController
     
     @IBAction func buttonPressed(_ sender: Any)
     {
-        if self.currentIndex < self.favoriteFoodsArray.count
         //Line 37 is checking the conditionof currentIndex number and comparing it to the overall array total which in this case is 4. If crrentindex is less than favoriteFoodsArray count it will run the if but if current index is greater than favoriteFoodsArray it will run the else statement
-        {
-            self.bottomLabel.text = favoriteFoodsArray[currentIndex]
-            //line 40 is setting bottomLabel to the current favorit food string in corispondence to the current index upon IBAction
-            buttonLabel.setTitle("Next", for : UIControl.State.normal)
-            //This line sets the title of the button to "next" as long as the if statment is running and their are more iterations to cycle through
-            currentIndex+=1
-            //This line increases the Currentindex intiger by 1 each time the button is pressed it effects the global var currentIndex previously set to 0
+        if self.currentIndex < self.favoriteFoodsArray.count
         
+        {
+            //line 40 is setting bottomLabel to the current favorit food string in corispondence to the current index upon IBAction
+            self.bottomLabel.text = favoriteFoodsArray[currentIndex]
+            
+            //This line sets the title of the button to "next" as long as the if statment is running and their are more iterations to cycle through
+            buttonLabel.setTitle("Next", for : UIControl.State.normal)
+            
+            //This line increases the Currentindex intiger by 1 each time the button is pressed it effects the global var currentIndex previously set to 0
+            currentIndex+=1
+            
         }
         else
         {
-            print("Button has been Disabled")
             //This print places the above string in the consol establising the buton has been disabled
+            print("Button has been Disabled")
+           
+            //This line Disables the button by turning it off completly
             (buttonLabel!).isEnabled=false
-            //This line Disables the button by turning it off completly 
+            
+        
         }
     }
     
